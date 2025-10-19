bbbda2a28bda8ee92f49a23bba3e1992d88baab0
Joeri Abbo <jabbo@schubergphilis.com>
2025-10-19 22:24:10 +0200

    Add fix for if version does not exist yet to have an overwrite.

--- commands/datadog_bump_components.py
+++ commands/datadog_bump_components.py
@@ -1,7 +1,7 @@
 import click
 
 from lizzy.cli import BaseCommand
-from lizzy.datadog import get_fetch_versions
+from lizzy.datadog import get_fetch_versions, bump_datadog_components as bump_datadog_components_now
 
 
 class DatadogBumpComponentsCommand(BaseCommand):
@@ -14,12 +14,15 @@ class DatadogBumpComponentsCommand(BaseCommand):
             """Bump Datadog components to the latest version."""
             click.echo("Bumping Datadog components...")
             version = click.prompt("Give me the Datadog version to bump to")
-            versions = get_fetch_versions()
-            if version not in versions:
-                click.echo(f"Version {version} not found in available versions.")
-                click.echo("Available versions are:")
-                for v in versions:
-                    click.echo(f"- {v}")
-                return
-            bump_datadog_components(version)
+            if input(f"Do you want to check if version {version} exists? (y/n)") == "y":
+                versions = get_fetch_versions()
+                if version not in versions:
+                    click.echo(f"Version {version} not found in available versions.")
+                    click.echo("Available versions are:")
+                    for v in versions:
+                        click.echo(f"- {v}")
+                    if input("Do you still want to proceed? (y/n)") != "y":
+                        click.echo("Aborting bump operation.")
+                        return
+            bump_datadog_components_now(version)
             click.echo(f"Datadog components bumped to version {version}.")
