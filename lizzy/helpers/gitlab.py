e1bb54ad1b776437e5f0c9e7044863d665acd006
Joeri Abbo <34270045+Joeri-Abbo@users.noreply.github.com>
2025-11-20 20:53:32 +0100

    Feature/big refactor (#39)
    * Wip refactor
    
    * Update
    
    * Bump
    
    * Move from Linux to MacOS
    
    * Bump
    
    * Add fixes?
    
    * Add merge yolo

--- lizzy/helpers/gitlab.py
+++ lizzy/helpers/gitlab.py
@@ -90,49 +90,52 @@ def fetch_approved_merge_requests(yolo: bool = False) -> None:
 
     projects = group.projects.list(include_subgroups=True, all=True)
     for project in projects:
-        click.echo(f"Found project: {project.name}, scanning for approved MRs...")
-        proj = gl.projects.get(project.id)
-        merge_requests = proj.mergerequests.list(state="opened", all=True)
-        if not merge_requests:
-            click.echo(f"No open merge requests found for project: {project.name}")
-            continue
-
-        for mr in merge_requests:
-            if mr.author["username"] != username:
-                click.echo(f"Skipping MR {mr.title} by {mr.author['username']}")
+        try:
+            click.echo(f"Found project: {project.name}, scanning for approved MRs...")
+            proj = gl.projects.get(project.id)
+            merge_requests = proj.mergerequests.list(state="opened", all=True)
+            if not merge_requests:
+                click.echo(f"No open merge requests found for project: {project.name}")
                 continue
 
-            mr_detail = proj.mergerequests.get(mr.iid)
-            pipelines = mr_detail.pipelines.list(per_page=1,get_all=True)
+            for mr in merge_requests:
+                if mr.author["username"] != username:
+                    click.echo(f"Skipping MR {mr.title} by {mr.author['username']}")
+                    continue
 
-            if not pipelines:
-                click.echo(f"No pipelines for MR {mr.title}")
-                continue
-            pipeline = pipelines[0]
+                mr_detail = proj.mergerequests.get(mr.iid)
+                pipelines = mr_detail.pipelines.list(per_page=1,get_all=True)
 
-            if pipeline.status != "success":
-                click.echo(f"MR {mr.title} has failed jobs, skipping")
-                continue
+                if not pipelines:
+                    click.echo(f"No pipelines for MR {mr.title}")
+                    continue
+                pipeline = pipelines[0]
 
-            approvals = mr_detail.approvals.get()
+                if pipeline.status != "success":
+                    click.echo(f"MR {mr.title} has failed jobs, skipping")
+                    continue
 
-            success = False
-            for approver in approvals.approved_by:
-                if approver["user"]["username"] != username:
-                    success = True
-                    click.echo(
-                        f"MR {mr.title} approved by {approver['user']['username']} created by {mr.author['username']}"
-                    )
-                    break
-            if success:
-                click.echo(f"Found approved merge request: {mr.title} ({mr.web_url})")
-                if yolo:
-                    click.echo(f"Auto-merging MR {mr.title}")
-                    mr_detail.merge()
-                else:
-                    if input(f"Merge {mr.title}? (y/n): ").lower() == "y":
-                        try:
-                            mr_detail.merge()
-                            click.echo(f"Merged MR: {mr.title}")
-                        except Exception as e:
-                            click.echo(f"Failed to merge MR {mr.title}: {e}")
+                approvals = mr_detail.approvals.get()
+
+                success = False
+                for approver in approvals.approved_by:
+                    if approver["user"]["username"] != username:
+                        success = True
+                        click.echo(
+                            f"MR {mr.title} approved by {approver['user']['username']} created by {mr.author['username']}"
+                        )
+                        break
+                if success:
+                    click.echo(f"Found approved merge request: {mr.title} ({mr.web_url})")
+                    if yolo:
+                        click.echo(f"Auto-merging MR {mr.title}")
+                        mr_detail.merge()
+                    else:
+                        if input(f"Merge {mr.title}? (y/n): ").lower() == "y":
+                            try:
+                                mr_detail.merge()
+                                click.echo(f"Merged MR: {mr.title}")
+                            except Exception as e:
+                                click.echo(f"Failed to merge MR {mr.title}: {e}")
+        except Exception as e:
+            click.echo(f"Error processing project {project.name}: {e}")
